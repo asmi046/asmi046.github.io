b3f991cb3d451ef928860b0657cbd5f237f344e4
ruvick <rudikov.web@ya.ru>
2020-08-28 23:13:04 +0300

    Залил Бургер меню на 520
    Залил Бургер меню на 520

--- elisymba/js/custom.js
+++ elisymba/js/custom.js
@@ -37,3 +37,17 @@ jQuery(document).ready(function ($) {
     $('.mob-menu').css('bottom', '140%');
   });
 });
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach (icon => {  
+  icon.addEventListener('click', (event) => {
+    icon.classList.toggle("open");
+  });
+});
+
+$(function () {
+  $('.nav-icon-5').click(function () {
+    $('.menu-list').toggleClass('show-menu')
+    $('body').toggleClass('lock')
+  })  
+});
